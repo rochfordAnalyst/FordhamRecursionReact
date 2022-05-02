732de0d98ad4667e6d12d29a082afa224c757c78
rochfordAnalyst <brochford@fordham.edu>
2022-05-02 11:11:31 -0400

    newest

--- src/pages/index.js
+++ src/pages/index.js
@@ -138,13 +138,7 @@ const IndexPage = () => {
           🎉🎉🎉
         </span>
       </h1>
-      <p style={paragraphStyles}>
-        Edit <code style={codeStyles}>src/pages/index.js</code> to see this page
-        update in real-time.{" "}
-        <span role="img" aria-label="Sunglasses smiley emoji">
-          😎
-        </span>
-      </p>
+      
       <ul style={listStyles}>
         <li style={docLinkStyle}>
           <a
